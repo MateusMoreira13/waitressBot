f3ce51dc59616a8309290637dc34542fb3a1c5c8
Mateus R. Moreira <mateusrmoreira20@yahoo.com>
2021-05-13 16:10:49 -0300

    adjustes done

--- main.go
+++ main.go
@@ -8,27 +8,30 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+var b, err = tb.NewBot(tb.Settings{
+	// You can also set custom API URL.
+	// If field is empty it equals to "https://api.telegram.org".
+
+	Token:  "1849565820:AAH1--d2jzrkmOkF251s91RDXu6WbljAC0U",
+	Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+})
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		panic(port)
 	}
-	b, err := tb.NewBot(tb.Settings{
-		// You can also set custom API URL.
-		// If field is empty it equals to "https://api.telegram.org".
-
-		Token:  "1849565820:AAH1--d2jzrkmOkF251s91RDXu6WbljAC0U",
-		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
-	})
 
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	b.Handle("/hello", func(m *tb.Message) {
-		b.Send(m.Sender, "Hello World!")
-	})
+	b.Handle("/hello", hello)
 
 	b.Start()
+}
+
+func hello(m *tb.Message) {
+	b.Send(m.Sender, "Hello World!")
 }
